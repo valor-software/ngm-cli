ab0fb0f44bcab9aa5ef9166706e8d976b47858f2
Dmitriy Schekhovtsov <valorkin@gmail.com>
2016-12-15 14:56:39 +0200

    fix(ngm-cli): use ngc, why it is tsc again

--- src/ngm/tasks/build.task.ts
+++ src/ngm/tasks/build.task.ts
@@ -1,5 +1,5 @@
 const execa = require('execa');
 
 export function build(project:string) {
-    return execa('tsc', ['-p', project], {preferLocal: true});
+    return execa('ngc', ['-p', project], {preferLocal: true});
 }
